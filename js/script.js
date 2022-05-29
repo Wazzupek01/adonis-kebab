395d4436310ddcfc3f36ba0f9bdf216791204584
Wazzupek01 <wazzupekgaming@gmail.com>
2022-05-29 12:18:03 +0200

    More responsive and mobile friendly navbar, better placement of jumbotron buttons, attempt to make ordering form

--- js/script.js
+++ js/script.js
@@ -1,15 +1,22 @@
 $(function () {
     window.addEventListener("resize", function() {
         if (window.innerWidth < 520) {
-            $("#btn-zamow").addClass("hidden");
-            $("#btn-kontakt").addClass("hidden");
+            $("#btn-order").addClass("hidden");
+            $("#btn-contact").addClass("hidden");
         } else if (window.innerWidth < 980 && window.innerWidth >= 520){
-            $("#btn-zamow").addClass("btn-sm").removeClass("btn-lg hidden");
-            $("#btn-kontakt").addClass("btn-sm").removeClass("btn-lg hidden");
+            $("#btn-order").addClass("btn-sm").removeClass("btn-lg hidden");
+            $("#btn-contact").addClass("btn-sm").removeClass("btn-lg hidden");
         } else if (window.innerWidth >= 980) {
-            $("#btn-zamow").addClass("btn-lg").removeClass("btn-sm");
-            $("#btn-kontakt").addClass("btn-lg").removeClass("btn-sm");
+            $("#btn-order").addClass("btn-lg").removeClass("btn-sm");
+            $("#btn-contact").addClass("btn-lg").removeClass("btn-sm");
         }
     });
 
+    $("#btn-order").click(function() {
+        let order = $("#order");
+        if(order.is(":empty")) {
+            for (let i = 0; i < 10; i++)
+                order.append(`<div class='item'> pozycja ${i+1} <input type='checkbox'><span class='item-checkbox'></span></div>`);
+        }
+    });
 })
